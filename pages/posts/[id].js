fda53a4430909c834a528de685ce93d0c8f71c57
jianwu <jianwu.wang@ximalaya.com>
2022-05-02 19:31:49 +0800

    Next Learning

---
+++ pages/posts/[id].js
@@ -0,0 +1,45 @@
+import Head from 'next/head';
+import Layout from '../components/layout';
+import Date from '../components/date';
+import { getAllPostIds, getPostData } from '../../lib/posts';
+import utilStyles from '../../styles/utils.module.css';
+import { useRouter } from 'next/router';
+
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
+    </Layout>
+  );
+}
+
+export async function getStaticPaths() {
+  const paths = getAllPostIds();
+
+  return {
+    paths,
+    // If fallback is false, then any paths not returned by getStaticPaths will result in a 404 page.
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params }) {
+  // Add the "await" keyword like this:
+  const postData = await getPostData(params.id);
+
+  return {
+    props: {
+      postData,
+    },
+  };
+}
